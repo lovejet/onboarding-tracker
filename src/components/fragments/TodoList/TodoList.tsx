c2f1a31310a58797622c81850f1f648b6e05b9c2
Harold <haroldhong.love@gmail.com>
2022-02-20 12:22:07 -0600

    remove needless code

--- src/components/fragments/TodoList/TodoList.tsx
+++ src/components/fragments/TodoList/TodoList.tsx
@@ -1,7 +1,6 @@
 
 import React from "react"
 import { useDispatch, useSelector } from "react-redux";
-import Checkbox from '@mui/material/Checkbox';
 
 import { selectTodoList, setTodoItemCompleted } from "@redux-reducers/todos";
 
